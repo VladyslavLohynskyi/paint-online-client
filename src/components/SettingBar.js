e2514e9a58896983e8b64a48ffa1900c3fbc2009
VladyslavLohynskyi <watcher.good2003@gmail.com>
2022-03-09 20:05:41 +0200

    added structure of client,added some scss styles

---
+++ src/components/SettingBar.js
@@ -0,0 +1,7 @@
+import "../styles/toolbar.scss";
+
+function SettigBar() {
+  return <div className="settingbar" style={{ top: 40 }}></div>;
+}
+
+export default SettigBar;
---
+++ src/components/Toolbar.js
@@ -0,0 +1,19 @@
+import "../styles/toolbar.scss";
+
+function Toolbar() {
+  return (
+    <div className="toolbar">
+      <button className="toolbar__btn brush"></button>
+      <button className="toolbar__btn rect"></button>
+      <button className="toolbar__btn circle"></button>
+      <button className="toolbar__btn eraser"></button>
+      <button className="toolbar__btn line"></button>
+      <input style={{ marginLeft: 10, cursor: "pointer" }} type="color" />
+      <button className="toolbar__btn undo"></button>
+      <button className="toolbar__btn redo"></button>
+      <button className="toolbar__btn save"></button>
+    </div>
+  );
+}
+
+export default Toolbar;
