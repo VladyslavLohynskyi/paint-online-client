58c22bb84bace56be5db1725c0fe4bbba076ccba
VladyslavLohynskyi <watcher.good2003@gmail.com>
2022-03-09 20:49:03 +0200

    added functionality for brush

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,11 +1,21 @@
+import { observer } from "mobx-react-lite";
+import { useEffect, useRef } from "react";
+import canvasState from "../store/canvasState";
+import toolState from "../store/toolState";
 import "../styles/canvas.scss";
+import Brush from "../tools/Brush";
 
-function Canvas() {
+const Canvas = observer(() => {
+  const canvasRef = useRef();
+  useEffect(() => {
+    canvasState.setCanvas(canvasRef.current);
+    toolState.setTool(new Brush(canvasRef.current));
+  }, []);
   return (
     <div className="canvas">
-      <canvas width={600} height={400} />
+      <canvas ref={canvasRef} width={600} height={400} />
     </div>
   );
-}
+});
 
 export default Canvas;
